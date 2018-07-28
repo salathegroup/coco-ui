8481005cd935cac2407255c27e0fb0b4278ea474
Seth Vanderwilt <seth.vanderwilt@gmail.com>
2018-07-28 10:33:06 +0200

    Rename balance checking script

--- check_account_balance.py
+++ check_account_balance.py
@@ -6,7 +6,7 @@ import boto3
 aws_key = json.load(open("keys.json"))
 
 
-MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+MTURK_SANDBOX = 'https://mturk-requester.us-east-1.amazonaws.com'
 mturk = boto3.client('mturk',
                      aws_access_key_id=aws_key["aws_access_key_id"],
                      aws_secret_access_key=aws_key["aws_secret_access_key"],
@@ -14,6 +14,6 @@ mturk = boto3.client('mturk',
                      endpoint_url=MTURK_SANDBOX
                      )
 
-balance = mturk.get_account_balance()['AvailableBalance']  # should be $10000
+balance = mturk.get_account_balance()['AvailableBalance']  # should be $10000 if sandbox
 
 print("Available balance for AWS key=%s = %s" % (aws_key["aws_access_key_id"], balance))
